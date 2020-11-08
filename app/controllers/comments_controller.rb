a16c70bf0ceb06222f43eae5528b24c5aafc82bb
minamihiroto <kokowatamf@gmail.com>
2020-11-08 23:52:32 +0900

    ✨ Add main_page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
   def index
   end
+
+  def main
+    @users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'comments#index'
   get 'comments/index'
+  get 'comments/main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
