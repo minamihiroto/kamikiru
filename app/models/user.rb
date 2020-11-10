ae0fddaf58a39213548d4d38410de3d12dc2bdc1
minamihiroto <kokowatamf@gmail.com>
2020-11-10 18:24:44 +0900

    ✨ Add need for Active Storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 追記
+  has_many_attached :images
 end
