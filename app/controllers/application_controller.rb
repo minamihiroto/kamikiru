712c70b75bacedba3726f097479eda452c1a7ed0
minamihiroto <kokowatamf@gmail.com>
2020-11-13 01:37:53 +0900

    ✨ add images form (new,edit)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:images]) # 新規登録時(sign_up時)にimagesが必要
     devise_parameter_sanitizer.permit(:account_update, keys: [:name]) # nameの編集を許可する
+    devise_parameter_sanitizer.permit(:account_update, keys: [:images]) # imagesの編集を許可する
 
   end
   # ここまで追加します
