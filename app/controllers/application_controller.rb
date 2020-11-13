f342a5c7dbc06d6c6b69cb4bade7f0a7ea076570
minamihiroto <kokowatamf@gmail.com>
2020-11-13 18:57:41 +0900

    🐛 images周りの記述を変更し、登録、編集ができるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:images]) # 新規登録時(sign_up時)にimagesが必要
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name]) # nameの編集を許可する
-    devise_parameter_sanitizer.permit(:account_update, keys: [:images]) # imagesの編集を許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, images: []]) # 新規登録時(sign_up時)にname,imagesというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, images: []]) # name,imagesの編集を許可する
 
   end
   # ここまで追加します
