07f08dc3dc1c974b4e3a212cfb6d6f813e4f45c8
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-11-23 14:48:44 +0900

    🔖 マッチング機能のメソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,19 @@ class User < ApplicationRecord
   has_many :reverse_of_machings, class_name: 'Maching', foreign_key: :requested_user_id
   has_many :requested_users, through: :reverse_of_machings, source: :requested_user
 
+  def request(other_user)
+    unless self == other_user
+      self.machings.find_or_create_by(request_id: other_user.id)
+    end
+  end
+
+  def unrequest(other_user)
+    relationship = self.machings.find_by(request_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def requesting?(other_user)
+    self.requestings.include?(other_user)
+  end
+
 end
