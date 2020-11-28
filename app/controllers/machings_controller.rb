0c643b22f86bfa0dc92d99e22d53e105b77ed81b
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-11-29 01:26:49 +0900

    🐛 リダイレクト先とパラメーターの記述変更

--- app/controllers/machings_controller.rb
+++ app/controllers/machings_controller.rb
@@ -5,10 +5,10 @@ class MachingsController < ApplicationController
         requesting = current_user.request(@user)
         if requesting.save
           flash[:success] = 'ユーザーに依頼しました'
-          redirect_to @user
+          redirect_to comments_main_path
         else
           flash.now[:alert] = '依頼に失敗しました'
-          redirect_to @user
+          redirect_to comments_main_path
         end
     end
 
@@ -17,16 +17,17 @@ class MachingsController < ApplicationController
     #     requesting = current_user.unrequest(@user)
     #     if requesting.destroy
     #       flash[:success] = '依頼を取り消しました'
-    #       redirect_to @user
+    #       redirect_to comments_main_path
     #     else
     #       flash.now[:alert] = '依頼の取り消しに失敗しました'
-    #       redirect_to @user
+    #       redirect_to comments_main_path
     #     end
     # end
 
+    # このコントローラ内でしか使わないためprivateと記述
     private
-    def
-        @user = User.find(params[:maching][:request_id])
+    def set_user
+        # リクエストされるユーザーのデータが入る
+        @user = User.find params[:request_id]
     end
-
 end
