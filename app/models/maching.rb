75ac9858dd04f8a81ea3cde9bc57940142ef1334
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-11-29 01:21:55 +0900

    🔖 コメントを追加

--- app/models/maching.rb
+++ app/models/maching.rb
@@ -1,6 +1,9 @@
 class Maching < ApplicationRecord
+    # belongs_toでusersテーブルとの関係を記述(1対)
+    # request_user,requested_userという架空のテーブルを参照しているが、実際はclass_name: 'User'でusersテーブルを参照していることになってる
     belongs_to :request_user, class_name: 'User'
     belongs_to :requested_user, class_name: 'User'
+    # presence: trueで空データを登録できないようにする
     validates :request_user_id, presence: true
     validates :requested_user_id, presence: true
 end
