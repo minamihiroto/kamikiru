b5f4c553637977356e1e40903b4e9c2d9fa581f6
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-11-30 00:27:40 +0900

    🔧 いらないコードを削除 説明文を記述したことによって判明

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     # 自分自身(current_user)が依頼したデータ(Machingテーブルのrequest_user_id)を参照する
     @request_user = Maching.request_user(current_user)
     # 自分自身(current_user)が依頼されたデータ(Machingテーブルのrequested_user_id)を参照する
-    @requested_user = Maching.requested_user(current_user).where.not(request_user_id: current_user)
+    @requested_user = Maching.requested_user(current_user)
   end
 
   def main
