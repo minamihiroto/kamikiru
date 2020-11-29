dee23cfa893ab4194fbad4db54f11a191721352f
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-11-29 23:47:18 +0900

    🔖 説明を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,18 @@ class User < ApplicationRecord
   has_many_attached :images
 
   # has_manyでmachingsテーブルとの関係を記述(多対）
-  # foreign_keyでは、matchingsテーブルのrequest_user_idカラムによって紐付けが行われていることを明示しています
+  # foreign_keyでは、machingsテーブルのrequest_user_idカラムによって紐付けが行われていることを明示しています
   has_many :machings, class_name: 'Maching', foreign_key: :request_user_id
-  # :request_usersは架空のモデルでmachingsテーブルの:request_userを参考にしている
-  # 中間テーブルをthorughで書くのは決り文句です
-  # sourceは関連付けてるmodelを明示しています。今回はrequest_usersモデルなので、source: :request_userとQiitaにも書いてありました。
-  # が、これは has_many :request_usersより明確なため、今回はなくても動くと思われます
-  has_many :request_users, through: :machings, source: :requested_user
-  # reverse_of_machingsは  has_many :machingsと名前を区別するために書いています。名前は何でもいいです。
-  # foreign_keyでは、matchingsテーブルのrequested_user_idカラムによって紐付けが行われていることを明示しています
+  # reverse_of_machingsはhas_many :machingsと名前を区別するために書いている
+  # foreign_keyでは、machingsテーブルのrequested_user_idカラムによって紐付けが行われていることを明示している
   has_many :reverse_of_machings, class_name: 'Maching', foreign_key: :requested_user_id
+
   # 中間テーブルをthorughで書くのは決り文句です
+  # request_usersで取得したいのはselfが依頼したユーザー、つまり「セルフに依頼されたユーザー」と言うことになる
+  # その場合参照元は(source)はmachingsの依頼されたユーザーを表しているrequested_userになり、参照先がrequest_usersになる多対多の関係を記述
+  has_many :request_users, through: :machings, source: :requested_user
+  # 次は逆で、requested_usersで取得したいのはselfが依頼されたユーザー、つまり「セルフに依頼したユーザー」と言うことになる
+  # その場合参照元は(source)はreverse_of_machingsの依頼したユーザーを表しているrequest_userになり、参照先がrequested_usersになる多対多の関係を記述
   has_many :requested_users, through: :reverse_of_machings, source: :request_user
 
   def request(other_user)
