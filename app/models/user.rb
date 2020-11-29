67be36fe5418227c8e21b8b27611a2da37096e9a
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-11-29 17:48:30 +0900

    🐛 依頼ボタンを押した際のアクションが正常化

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,19 +12,19 @@ class User < ApplicationRecord
   # 中間テーブルをthorughで書くのは決り文句です
   # sourceは関連付けてるmodelを明示しています。今回はrequest_usersモデルなので、source: :request_userとQiitaにも書いてありました。
   # が、これは has_many :request_usersより明確なため、今回はなくても動くと思われます
-  has_many :request_users, through: :machings
+  has_many :request_users, through: :machings, source: :requested_user
   # reverse_of_machingsは  has_many :machingsと名前を区別するために書いています。名前は何でもいいです。
   # foreign_keyでは、matchingsテーブルのrequested_user_idカラムによって紐付けが行われていることを明示しています
   has_many :reverse_of_machings, class_name: 'Maching', foreign_key: :requested_user_id
   # 中間テーブルをthorughで書くのは決り文句です
-  has_many :requested_users, through: :reverse_of_machings
+  has_many :requested_users, through: :reverse_of_machings, source: :request_user
 
   def request(other_user)
     # selfはこの定義を呼び出したユーザーのこと
     # 自分にリクエスト送らないようにする記述
     unless self == other_user
       # other_user.idがmachingsのrequested_user_idカラムに存在するなら取得する、存在しないなら新規作成する(保存はしない)
-      self.machings.find_or_initialize_by(request_user_id: self.id, requested_user_id: other_user.id)
+      self.machings.find_or_initialize_by(requested_user_id: other_user.id)
     end
   end
 
@@ -36,12 +36,12 @@ class User < ApplicationRecord
 
   # selfが依頼したユーザー一覧を取得できる
   def requesting?(other_user)
-    # request_usersは上記のhas_many :request_users, through: :machingsのこと
+    # self.request_users メソッドで取得したいのは、「Selfに依頼されたユーザー」一覧なので、扱われるModelは requested_user
     self.request_users.include?(other_user)
   end
 
   def requested?(other_user)
-    # request_usersは上記のhas_many :request_users, through: :machingsのこと
+    # self.requested_users メソッドで取得したいのは、「Selfに依頼したユーザー」一覧なので、扱われるModelは request_user
     self.requested_users.include?(other_user)
   end
 
