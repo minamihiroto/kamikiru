468eb42721a9f58c986f1f23579a6abaf6cf0177
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-12-05 15:04:53 +0900

    🔖 依頼してきたユーザーとマッチングが成立次第一覧から外されるように設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     # 自分自身(current_user)が依頼したデータ(Machingテーブルのrequested_user_id)を参照する
     @request_user = Maching.request_user(current_user)
     # 自分自身(current_user)が依頼されたデータ(Machingテーブルのrequest_user_id)を参照する
-    @requested_user = Maching.requested_user(current_user)
+    @requested_user = current_user.requested_users.eager_load(:machings).where(machings: { aggree: false })
     @users = User.all
     @approve = current_user.requested_users.eager_load(:machings).where(machings: { aggree: true })
   end
