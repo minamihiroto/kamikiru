506809f51facf7faa1c2ce9169ba68880e84ec5b
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-12-07 00:15:48 +0900

    ✨ コントローラにメッセージ送信のメソッドを作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
     def create
-
+        @message = Message.new(message_content: params[:message][:message_content],message_user_id: params[:message][:message_user_id],message_maching_id: params[:message][:message_maching_id])
+        @message.save!
+        redirect_to maching_path(@message.message_maching_id)
     end
 end
