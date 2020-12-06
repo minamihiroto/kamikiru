fd1bc9c63d2572b17449f65e75db1f6b5171b9f9
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-12-07 00:16:27 +0900

    🔧 ログアウト後ログイン画面に移行できるよう編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
   def index
+    if current_user
     @request_user = current_user.request_users.eager_load(:reverse_of_machings).where(machings: { aggree: false })
     @requested_user = current_user.requested_users.eager_load(:machings).where(machings: { aggree: false })
     @approve_request_user = current_user.request_users.eager_load(:reverse_of_machings).where(machings: { aggree: true })
     @approve_requested_user  = current_user.requested_users.eager_load(:machings).where(machings: { aggree: true })
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def main
