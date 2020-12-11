58c4f5c5abcafda540950b5f5ca3d23dda2449ef
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-12-11 23:44:05 +0900

    🐛 notificationマイグレーションファイル記述修正

--- db/migrate/20201210140028_create_notifications.rb
+++ db/migrate/20201210140028_create_notifications.rb
@@ -3,8 +3,8 @@ class CreateNotifications < ActiveRecord::Migration[6.0]
     create_table :notifications do |t|
       t.references :notification_message, foreign_key: { to_table: :messages }, comment: "DMのメッセージ"
       t.references :notification_user, null: false,　foreign_key: { to_table: :users }, comment: "通知を受けるユーザー"
-      t.references :notification_maching, foreign_key: { to_table: :maching }, comment: "DMしてる部屋"
-      t.references :notification_notification_type, null: false, foreign_key: { to_table: :notification_types }, comment: "お知らせの種類"
+      t.references :notification_maching, foreign_key: { to_table: :machings }, comment: "DMしてる部屋"
+      t.integer :notification_notification_type_id, null: false, comment: "お知らせの種類"
       t.boolean :notification_read, default: false, comment: "既読判定"
       t.timestamps
     end
