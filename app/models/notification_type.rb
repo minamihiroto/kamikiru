b42900f6fec58b7d92ee4d8cbdea47a0301b694f
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-12-11 23:14:07 +0900

    ✨ notification_typeモデルを作成 データは固定されているため、マイグレーションファイルは不要（削除済み）

---
+++ app/models/notification_type.rb
@@ -0,0 +1,7 @@
+class NotificationType < ActiveHash::Base
+  self.data = [
+    {id: 1, name: 'マッチング依頼', notify_message: "マッチング依頼が届きました"}, 
+    {id: 2, name: 'マッチング承認', notify_message: "マッチングが承認されました"}, 
+    {id: 3, name: 'メッセージ到着', notify_message: "メッセージが届きました"}, 
+  ]
+end
