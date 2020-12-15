080ec290607e916f48e99d596814c75a45bad053
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-12-15 23:36:51 +0900

    🔖 依頼・承認が来たことを知らせる通知機能を追記

--- app/controllers/machings_controller.rb
+++ app/controllers/machings_controller.rb
@@ -2,27 +2,33 @@ class MachingsController < ApplicationController
     before_action :set_user,only:[:create]
 
     def create
+      # 依頼アクション
       # @userはrequestメソッド(user.rbのメソッド)のother_userにはいる（引数に入る）
-        requesting = current_user.request(@user)
-        if requesting.save
-          flash[:success] = 'ユーザーに依頼しました'
-          redirect_to comments_main_path
-        else
-          flash.now[:alert] = '依頼に失敗しました'
-          redirect_to comments_main_path
-        end
+      requesting = current_user.request(@user)
+      if requesting.save
+        flash[:success] = 'ユーザーに依頼しました'
+        Notification.create(notification_user_id: @user.id, notification_notification_type_id: 1)
+        redirect_to comments_main_path
+      else
+        flash.now[:alert] = '依頼に失敗しました'
+        redirect_to comments_main_path
+      end
     end
 
     def update
+      # 承認アクション
       # ログインしてるユーザー(requested_user)に依頼したユーザー(request_user)のマッチングを取得
       # ログインしてるユーザーが中間テーブル(「セルフ（ログインしてるユーザー）に依頼したユーザー」とのマッチング)を呼び出し、依頼してきたユーザーを引数に入れている
       maching = current_user.reverse_of_machings.find_by(id: params[:id])
       maching.update(aggree: true)
+      recive_notice_user = maching.request_user_id
+      Notification.create(notification_user_id: recive_notice_user, notification_notification_type_id: 2)
       redirect_to root_path
     end
 
     def show
       @maching = Maching.find_by(id: params[:id])
+       # メッセージアクション
       if @maching.request_user == current_user || @maching.requested_user == current_user
         @messages = Message.where(message_maching_id: params[:id])
         @message = Message.new
