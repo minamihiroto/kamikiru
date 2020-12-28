532df5906a3189b2f8b391a1fffecdd85794f7b9
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2020-12-29 01:49:52 +0900

    🔖 メニューコントローラ、ビューを編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,8 @@ class CommentsController < ApplicationController
     @requested_user = current_user.requested_users.eager_load(:machings).where(machings: { aggree: false })
     @approve_request_user = current_user.request_users.eager_load(:reverse_of_machings).where(machings: { aggree: true })
     @approve_requested_user  = current_user.requested_users.eager_load(:machings).where(machings: { aggree: true })
+    @menu = Menu.new
+    @menus = current_user.menus
     else
       redirect_to new_user_session_path
     end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,22 @@
 class MenusController < ApplicationController
+  def create
+    @request_user = current_user.request_users.eager_load(:reverse_of_machings).where(machings: { aggree: false })
+    @requested_user = current_user.requested_users.eager_load(:machings).where(machings: { aggree: false })
+    @approve_request_user = current_user.request_users.eager_load(:reverse_of_machings).where(machings: { aggree: true })
+    @approve_requested_user  = current_user.requested_users.eager_load(:machings).where(machings: { aggree: true })
+    @menus = current_user.menus
+    @menu = Menu.new(name: params[:menu][:name],fee: params[:menu][:fee],user_id: current_user.id)
+    if @menu.valid?
+      @menu.save
+      redirect_to root_path
+    else
+      render "comments/index"
+    end
+  end
+
+  def destroy
+    @menu = Menu.find(params[:id])
+    @menu.destroy
+    redirect_to root_path
+  end
 end
