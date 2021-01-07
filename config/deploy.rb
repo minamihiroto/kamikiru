c897ae338fede5a416cf12bf9ad57140a7d06ca8
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2021-01-07 22:33:03 +0900

    🔧 記法が古かったので修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url,  'git@github.com:minamihiroto/kamikiru.git'
 set :deploy_to, '/var/www/rails/kamikiru'
 
 # シンボリックリンクをはるファイル
-set :linked_files, fetch(:linked_files, []).push('config/master.key')
+append :linked_files, "config/master.key"
 
 # シンボリックリンクをはるフォルダ
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
