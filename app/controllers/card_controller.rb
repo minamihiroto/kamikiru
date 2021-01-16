4af688b40ba9464e41e182d1a75d05449adba3c7
南寛人 <hiroto@minamihirotonoMacBook-Pro.local>
2021-01-16 17:52:59 +0900

    🔖 pay.jpのコントローラを編集

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,7 +1,52 @@
 class CardController < ApplicationController
+
+  require "payjp"
+
   def new
+    card = Card.where(user: current_user.id)
+    redirect_to action: "show" if card.exists?
   end
 
-  def show
+  def pay
+    Payjp.api_key = Rails.application.credentials.payjp[:private_key]
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+      description: '登録テスト',
+      email: current_user.email,
+      card: params['payjp-token'],
+      metadata: {user: current_user.id}
+      ) #念の為metadataにuserを入れましたがなくてもOK
+      @card = Card.new(user: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
+  end
+
+  def delete #PayjpとCardデータベースを削除します
+    card = Card.where(user: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = Rails.application.credentials.payjp[:private_key]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "new"
+  end
+
+  def show #Cardのデータpayjpに送り情報を取り出します
+    card = Card.where(user: current_user.id).first
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = Rails.application.credentials.payjp[:private_key]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
   end
 end
